1bfa8cbb334dada14aaa1fd90c543f2899096520
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-01-12 20:03:22 +0100

    Implement getComputerChoice()

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-/*
-test
+
+/*test
 console.log("Hello, World!");
 */
 
+function getComputerChoice() {
+    //the function, getComputerChoice randomly returns computer choice of either "rock", "paper", or "scissors"
+    const max = 3, min = 1;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    const computerChoice = (randomNumber === 1)
+        ? "scissors" : (randomNumber === 2)
+        ? "rock" : "paper";
+    //console.log(`computer choice is ${randomNumber}: ${computerChoice}`);
+    return computerChoice;
+}
+
+getComputerChoice();
