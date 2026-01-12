961b6bdb9954fe374ac234d3b00172099eb769ee
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-01-12 20:20:54 +0100

    Add brackets to the conditions in ternary operator in getHumanChoice() for ease of reading

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function getComputerChoice() {
 function getHumanChoice() {
     //the function takes the player's input (a number between 1 and 3) and returns either "rock", "paper", or "scissors"
     const playerInput = parseInt(prompt(`1 is "rock", 2 is "paper", and 3 is "scissors". Enter your choice`));
-    const playerChoice = playerInput === 1
-        ? "rock" : playerInput === 2
-        ? "paper" : playerInput === 3
+    const playerChoice = (playerInput === 1)
+        ? "rock" : (playerInput === 2)
+        ? "paper" : (playerInput === 3)
         ? "scissors" : "It is invalid!";
     //console.log(`Your choice is ${playerInput}: ${playerChoice}`);
     return playerChoice;
