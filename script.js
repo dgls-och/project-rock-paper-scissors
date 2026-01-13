05bf1d8a897fc8c5e908ea9095c5487c28df7d18
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-01-13 21:10:39 +0100

    Add capitalizeFirstLetter() to capitalize first letter in players' choices on announcement, edit playRound() parameters to be case insensitive, and edit playRound() return values so that players' choices are capitalized on announcement

--- script.js
+++ script.js
@@ -31,21 +31,35 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+//capitalize each player's choice in announcement
+function capitalizeFirstLetter(playerChoice) {
+    const firstLetter = playerChoice.slice(0, 1);
+    const firstLetterToUpperCase = firstLetter.toUpperCase();
+    return `${firstLetterToUpperCase}${playerChoice.slice(1)}`;
+}
+//console.log(capitalizeFirstLetter(getComputerChoice()));
+const computer = capitalizeFirstLetter(getComputerChoice());
+const human = capitalizeFirstLetter(getHumanChoice());
+
 function playRound(humanChoice, computerChoice) {
     //playRound takes getComputerChoice() and getHumanChoice() as arguments, plays a round, increments the winner's score and prints a winner announcement
     //rock beats scissors, scissors beats paper, and paper beats rock
+
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
     if (humanChoice === computerChoice) {
         return `It is a draw.`;
     } else if (humanChoice === "rock" && computerChoice === "scissors"
         || humanChoice === "scissors" && computerChoice === "paper"
         || humanChoice === "paper" && computerChoice === "rock") {
             humanScore++;
-            return `${humanChoice} beats ${computerChoice}. You win!`;
+            return `${human} beats ${computer}. You win!`;
         } else if (humanChoice === "It is invalid!") {
             return "Your choice is invalid.";
         } else {
             computerScore++;
-            return `${computerChoice} beats ${humanChoice}! You loose.`;
+            return `${computer} beats ${human}! You loose.`;
         }
 }
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(playRound(human, computer));
