b456467bd91e2ceee74e6da2c9091aff0e2cda02
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-01-17 08:56:52 +0100

    Remove unnecessary comments making code more readable

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-
-/*test
-console.log("Hello, World!");
-*/
-
 function getComputerChoice() {
-    //the function randomly returns computer choice of either "rock", "paper", or "scissors"
     const max = 3, min = 1;
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    const computerChoice = (randomNumber === 1)
-        ? "scissors" : (randomNumber === 2)
-        ? "rock" : "paper";
-    //console.log(`computer choice is ${randomNumber}: ${computerChoice}`);
+    const computerChoice = (randomNumber === 1) ? "scissors" :
+        (randomNumber === 2) ? "rock" : "paper";
     return computerChoice;
 }
-//getComputerChoice();
 
 function getHumanChoice() {
-    //the function takes the player's input (a number between 1 and 3) and returns either "rock", "paper", or "scissors"
-    const playerInput = parseInt(prompt(`1 is "rock", 2 is "paper", and 3 is "scissors". Enter your choice`));
-    const playerChoice = (playerInput === 1)
-        ? "rock" : (playerInput === 2)
-        ? "paper" : (playerInput === 3)
-        ? "scissors" : "It is invalid!";
-    //console.log(`Your choice is ${playerInput}: ${playerChoice}`);
+    const playerInput = parseInt(
+        prompt(`1 is "rock", 2 is "paper", and 3 is "scissors". Enter your choice`)
+    );
+    const playerChoice = (playerInput === 1) ? "rock" : (playerInput === 2) ?
+        "paper" : (playerInput === 3) ? "scissors" : "It is invalid!";
     return playerChoice;
 }
-//getHumanChoice();
 
 function playGame() {
-    //playGame() calls playRound(), tracks score and declares winner
+    //tracks score and declares winner
 
-    //score trackers
     let humanScore = 0;
     let computerScore = 0;
 
@@ -38,7 +25,6 @@ function playGame() {
     function capitalizeFirstLetter(playerChoice) {
         return `${playerChoice.charAt(0).toUpperCase()}${playerChoice.slice(1)}`;
     }
-    //console.log(capitalizeFirstLetter(getComputerChoice()));
 
     for (let round = 1; round <= 5; round++) {
         const computer = getComputerChoice();
@@ -49,7 +35,6 @@ function playGame() {
     }
 
     function playRound(humanChoice, computerChoice) {
-        //playRound takes getComputerChoice() and getHumanChoice() as arguments, plays a round, increments the winner's score and prints a winner announcement
         //rock beats scissors, scissors beats paper, and paper beats rock
 
         humanChoice = humanChoice.toLowerCase();
@@ -57,21 +42,23 @@ function playGame() {
 
         if (humanChoice === computerChoice) {
             return `It is a draw.`;
-        } else if (humanChoice === "rock" && computerChoice === "scissors"
-            || humanChoice === "scissors" && computerChoice === "paper"
-            || humanChoice === "paper" && computerChoice === "rock") {
+        } else if (humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "paper" ||
+            humanChoice === "paper" && computerChoice === "rock") {
                 humanScore++;
-                return `${capitalizeFirstLetter(getHumanChoice())} beats ${capitalizeFirstLetter(getComputerChoice())}. You win!`;
-            } else if (humanChoice === "It is invalid!" && computerChoice === "rock" || "paper" || "scissors") {
+                return `${capitalizeFirstLetter(getHumanChoice())} beats ${
+                    capitalizeFirstLetter(getComputerChoice())}. You win!`;
+        } else if (humanChoice === "It is invalid!" && computerChoice ===
+            "rock" || "paper" || "scissors") {
                 computerScore;
                 humanScore;
                 return "Your choice is invalid.";
-            } else {
-                computerScore++;
-                return `${capitalizeFirstLetter(getComputerChoice())} beats ${capitalizeFirstLetter(getHumanChoice())}! You loose.`;
-            }
+        } else {
+            computerScore++;
+            return `${capitalizeFirstLetter(getComputerChoice())} beats ${
+                capitalizeFirstLetter(getHumanChoice())}! You loose.`;
+        }
     }
-    //console.log(playRound(human, computer));
 
     if (humanScore > computerScore) {
         return `You win!
